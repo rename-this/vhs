ccc6dd80b82a9262b4caa58fd41caafec1ce064f
Andrew Hare <andrew@carbonrelay.com>
2020-10-19 13:18:04 -0600

    Fix middleware

--- testdata/http_middleware.bash
+++ testdata/http_middleware.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+trap exit INT
+
 while IFS="$\n" read -r line; do
+    >&2 echo "recieved ${line:0:1}"
     echo "${line:2}" | jq --compact-output ".body += \" [[hijacked ${line:0:1}]]\""
 done
